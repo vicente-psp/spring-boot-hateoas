8309d9c5049cc4520bf5fc3a7619749028f78f2c
Vicente Silva <vicente.pspenha@gmail.com>
2019-05-25 01:17:43 -0300

    Configuração inicial do projeto criado pelo spring initializr.

---
+++ src/test/java/com/vicente/springboothateoas/SpringBootHateoasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vicente.springboothateoas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootHateoasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

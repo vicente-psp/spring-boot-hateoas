ddd779c41e1dd4e4523263d9bd3e71665c57d168
Vicente Silva <vicente.pspenha@gmail.com>
2019-05-25 22:25:46 -0300

    Inclusão da anotação @Repository.

--- src/main/java/com/vicente/springboothateoas/repositories/PeopleRepository.java
+++ src/main/java/com/vicente/springboothateoas/repositories/PeopleRepository.java
@@ -2,6 +2,8 @@ package com.vicente.springboothateoas.repositories;
 
 import com.vicente.springboothateoas.entities.People;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PeopleRepository extends JpaRepository<People, Long> {
 }
